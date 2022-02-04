72479301004bdcb85a2a78271845b3f956c7965f
clemialgm <j3guan@ucsd.edu>
2022-02-03 18:22:17 -0800

    lib code that fails

--- SkillDemo3.java
+++ SkillDemo3.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo3{
+    public static int add(int n, int m){
+        return n+m;
+    }
+}
---
+++ SkillDemo3Test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemo3Test {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo3.add(1,3));
+    }
+
+}
